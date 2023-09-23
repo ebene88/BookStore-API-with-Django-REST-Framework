df82cc9ba3372fc7de9612da7fb632fc0e4572b9
ebene88 <abenezernachew50@gmail.com>
2023-09-23 10:40:43 +0300

    fix: bug fixed

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -143,11 +143,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
-# Permissions:
-# AllowAny
-# IsAuthenticated
-# IsAdminUser
-# IsAuthenticatedOrReadOnly
+
 
 
 CORS_ALLOWED_ORIGINS = [
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -12,11 +12,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
-    # path('', include('book.urls', namespace='book')),
     path('api/', include('api.urls', namespace='api')),
     path('api/users/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('docs/', include_docs_urls(title='BookAPI')),
+    path('api/docs/', include_docs_urls(title='BookAPI')),
 
 
 ]
